7990b04928cb28018e84ac25133852b18400644c
lw20144623 <1183862787@qq.com>
2023-08-01 10:37:55 +0800

    adjust dir architectures. add isprs datasets

--- module/datasets/isprsda.py
+++ module/datasets/isprsda.py
@@ -16,16 +16,16 @@ logger = logging.getLogger(__name__)
 
 class IsprsDA(BaseData):
     LABEL_MAP = OrderedDict(
-        BgClutter=0,
-        imp_surf=1,
-        building=2,
-        low_vege=3,
-        tree=4,
-        car=5,
+        # BgClutter=-1,
+        imp_surf=0,
+        building=1,
+        low_vege=2,
+        tree=3,
+        car=4,
         # clutter=5
     )
     COLOR_MAP = OrderedDict(
-        BgClutter=[0, 0, 0],
+        # BgClutter=[0, 0, 0],
         imp_surf=[255, 255, 255],
         building=[0, 0, 255],
         low_vege=[0, 255, 255],
@@ -35,8 +35,8 @@ class IsprsDA(BaseData):
     )
     PALETTE = np.asarray(list(COLOR_MAP.values())).reshape((-1,)).tolist()
     SIZE = (512, 512)
-    IGNORE_LABEL = 0
+    IGNORE_LABEL = -1
 
     def __init__(self, image_dir, mask_dir, transforms=None, label_type='id'):
         super().__init__(image_dir, mask_dir, transforms, label_type=label_type,
-                         offset=0, ignore_label=self.IGNORE_LABEL, num_class=len(self.LABEL_MAP))
+                         offset=-1, ignore_label=self.IGNORE_LABEL, num_class=len(self.LABEL_MAP))
--- module/gast/class_balance.py
+++ module/gast/class_balance.py
@@ -55,16 +55,16 @@ class ClassBalanceLoss(nn.Module):
     def _get_class_wight(self):
         prob = (1.0 - self.freq) / self.temperature
         prob = torch.softmax(prob, dim=0)
-        # _max, _ = torch.max(prob, dim=0, keepdim=True)
-        _max = torch.mean(prob, dim=0, keepdim=True)
+        _max, _ = torch.max(prob, dim=0, keepdim=True)
+        # _max = torch.mean(prob, dim=0, keepdim=True)
         prob_normed = prob / (_max + self.eps)      # 0 ~ 1
         return prob_normed
 
     def _get_pixel_difficulty(self, ce_loss, label_onehot):
         ce_loss = ce_loss.view(-1, 1)               # (b*h*w, 1)
         ce_loss_classwise = ce_loss * label_onehot  # (b*h*w, c)
-        # _max, _ = torch.max(ce_loss_classwise, dim=0, keepdim=True)     # (1, c)
-        _max = torch.mean(ce_loss_classwise, dim=0, keepdim=True)     # (1, c)
+        _max, _ = torch.max(ce_loss_classwise, dim=0, keepdim=True)     # (1, c)
+        # _max = torch.mean(ce_loss_classwise, dim=0, keepdim=True)     # (1, c)
         pixel_difficulty = ce_loss_classwise / (_max + self.eps)    # (b*h*w, c)
         pixel_difficulty = pixel_difficulty.sum(dim=1)      # (b*h*w,)
         return pixel_difficulty

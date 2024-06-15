19cd357629dda4552e417e462d3e8e6e2801df9d
liuwang <1183862787@qq.com>
2024-06-15 15:47:27 +0800

    add cross-spectral and cross-spatial scenes.

--- runs/dca/run_pRgb2potsdam.sh
+++ runs/dca/run_pRgb2potsdam.sh
@@ -1,6 +1,6 @@
 
 export CUDA_VISIBLE_DEVICES=3
-#python tools/train_src.py --config-path st.dca.pRgb2potsdam
+python tools/train_src.py --config-path st.dca.pRgb2potsdam
 
 python tools/train_ssl_dca.py --config-path st.dca.pRgb2potsdam \
   --ckpt-model log/dca/pRgb2potsdam/src/Potsdam_best.pth \
--- runs/dca/run_pRgb2vaihingen.sh
+++ runs/dca/run_pRgb2vaihingen.sh
@@ -1,6 +1,6 @@
 
 export CUDA_VISIBLE_DEVICES=4
-#python tools/train_src.py --config-path st.dca.pRgb2vaihingen
+python tools/train_src.py --config-path st.dca.pRgb2vaihingen
 
 python tools/train_ssl_dca.py --config-path st.dca.pRgb2vaihingen \
   --ckpt-model log/dca/pRgb2vaihingen/src/Vaihingen_best.pth \
